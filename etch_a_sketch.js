bdcddcb61402d3d680738fd6ad4733567135397e
sebidelamata <sebidelamata@gmail.com>
2022-12-06 21:48:27 -0700

    created sketch hover draw

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -12,22 +12,44 @@ function createCols(rowDiv, numCols){
         // create column
         let colDiv = document.createElement("div");
         // create col name
-        colDiv.classList = rowDiv.classList + "-Column-" + String(i + 1);
+        colDiv.id = rowDiv.id + "-Column-" + String(i + 1);
+        // create class name
+        colDiv.classList = "etch-square";
         // append to row
         rowDiv.appendChild(colDiv);
     }
 
 }
 
-for(let i = 0; i < numRows; i++){
-    // create the row
-    let rowDiv = document.createElement("div");
-    // create a row name
-    rowDiv.classList = "Row-" + String(i + 1);
-    // create cols
-    createCols(rowDiv, numCols);
-    // append to the container
-    etchContainer.appendChild(rowDiv);
+function createBoard(){
 
+    for(let i = 0; i < numRows; i++){
+        // create the row
+        let rowDiv = document.createElement("div");
+        // create a row name
+        rowDiv.id = "Row-" + String(i + 1);
+        rowDiv.classList = "row"
+        // create cols
+        createCols(rowDiv, numCols);
+        // append to the container
+        etchContainer.appendChild(rowDiv);
+
+    }
 
 }
+
+createBoard();
+
+// hover color change
+function mouseHoverAction(){
+    console.log(this);
+    this.style.backgroundColor = "red";
+}
+
+// select our etch squares
+etchSquares = document.querySelectorAll(".etch-square");
+
+// add listener
+etchSquares.forEach(square => {
+    square.addEventListener('mouseover', mouseHoverAction);
+})
